5b991ee2d87f36f463580b1f911736207ec79d00
kimmoongi <moongi0320@naver.com>
2025-03-18 21:43:27 +0900

    Initial commit

---
+++ src/test/java/kookmin/kuham/KuhamApplicationTests.java
@@ -0,0 +1,13 @@
+package kookmin.kuham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
